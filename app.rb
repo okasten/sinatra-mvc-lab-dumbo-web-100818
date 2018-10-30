1b4efcc78ec2ad342049d8534e7b3a1ae70087bf
Olivia Kasten <odkasten@gmail.com>
2018-10-30 16:40:18 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    text = PigLatinizer.new
+    @text = text.piglatinize(params[:user_phrase])
+
+    erb :results
+  end
 end
